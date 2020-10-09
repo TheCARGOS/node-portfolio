74552755513b00ee0160db1ca0f5f5c9771459c1
TheCARGOS <loayzavelez@gmail.com>
2020-10-08 23:32:46 -0500

    deployment files uploaded

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import app from "./app"
 
 async function init () {
     await startConnection()
-    app.listen(3000, () => [
-        console.log("Server is running on port 3000")
+    app.listen(process.env.PORT || 3000, () => [
+        console.log("Server is running on port " + app.get("port"))
     ])
 }
 
